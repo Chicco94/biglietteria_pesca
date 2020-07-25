bdc3951a7e06ddc06e9a9d2f28eab1d1c585375b
Chicco94 <enrico.cominato@gmail.com>
2020-07-25 10:58:44 +0200

    test

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ import os
 from flask import Flask, render_template
 import random
 
-BIGLIETTI = range(10)
+BIGLIETTI = [i for i in range(10)]
+
+BIGLIETTO_ESTRATTO = "Prendi un biglietto"
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', variable=BIGLIETTO_ESTRATTO)
 
-
-@app.route('/estrai',methods=['POST'])
-def estrai():
+@app.route('/background_process_test')
+def background_process_test():
     global BIGLIETTI
+    global BIGLIETTO_ESTRATTO
     random.shuffle(BIGLIETTI)
-    print(BIGLIETTI.pop())
+    BIGLIETTO_ESTRATTO = BIGLIETTI.pop()
+    print(BIGLIETTO_ESTRATTO)
+    return 'OK'
 
 if __name__ == '__main__':
-
     app.run()
 
 
